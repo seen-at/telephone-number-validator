7280ce39130fb01aed0a9efad227ffbe1ea28f5b
seen-at <90022384+seen-at@users.noreply.github.com>
2022-06-16 15:28:04 +0600

    Update and rename telephone-number-validator.txt to telephone-number-validator.js
    brute code

--- telephone-number-validator.js
+++ telephone-number-validator.js
@@ -18,33 +18,3 @@ function telephoneCheck(str) {
 }
 
 console.log(telephoneCheck("55 55-55-555-5"));
-
-// using regex loop
-
-function telephoneCheck(str) {
-  const validPatterns = [
-    // 555-555-5555
-    /^\d{3}-\d{3}-\d{4}$/,
-
-    // 1 555-555-5555
-    /^1 \d{3}-\d{3}-\d{4}$/,
-
-    // 1 (555) 555-5555
-    /^1 \(\d{3}\) \d{3}-\d{4}$/,
-
-    // 5555555555
-    /^\d{10}$/,
-
-    // (555)555-5555
-    /^\(\d{3}\)\d{3}-\d{4}$/,
-
-    // 1(555)555-5555
-    /^1\(\d{3}\)\d{3}-\d{4}$/,
-
-    // 1 555 555 5555
-    /^1 \d{3} \d{3} \d{4}$/, 
-  ] 
-  return validPatterns.some((patterns) => patterns.test(str));
-}
-
-telephoneCheck("555-555-5555");
